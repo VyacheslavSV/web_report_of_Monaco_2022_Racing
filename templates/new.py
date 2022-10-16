84e84879c5c50d9ca5ce3d5890481ff9b4429ce1
@foxstudent102022 <vyacheslavseregin21@gmail.com>
2022-10-16 15:26:32 +0300

    work cod flask

--- templates/new.py
+++
@@ -1,7 +0,0 @@
-from jinja2 import Template
-
-link = """{% raw %}In HTML-dok : <a hresf='#'>ss</a>{% endraw %}"""
-
-
-msg = Template(link)
-print(msg)
--- web_report.py
+++ web_report.py
@@ -1,30 +1,58 @@
-from flask import Flask, render_template
-from report import *
+from flask import Flask, render_template, request
+from svvs_report import build_report
+
+
+def drivers_cod_name_dict(data):
+    parsed_lines = []
+    for line in data:
+        abbr = str("".join(e[0] for e in str(line[0]).split())
+                   + "".join(e[0] for e in str(line[1]).split()))[0:3]
+        name = line[0]
+        stat = ', '.join(line)
+        dict_all = {'driver_id': abbr, 'name': name, abbr: stat}
+        parsed_lines.append(dict_all)
+    return parsed_lines
+
+
+def fine(k):
+    for dik in dict_all_info:
+        if dik['driver_id'] == k:
+            return dik.get(k)
+
 
 app = Flask(__name__)
 
 result = build_report('files')
-drivers = drivers_cod_name(build_report('files'))
+dict_all_info = drivers_cod_name_dict(result)
+dict_all_info_reverse = sorted(dict_all_info, key=lambda d: d['driver_id'], reverse=True)
 
 
 @app.route('/report')
 def index():
-    return render_template('report.html', title='Common statistic', result=result)
+    return render_template('report.html', title='Common statistic', diction=result)
 
 
-@app.route('/report/drivers')
+@app.route('/report/drivers/', methods=['GET'])
 def drivers():
-    return render_template('drivers.html', title='List of drivers name and code', drivers=drivers)
+    driver_id = request.args.get('driver_id')
+    order = request.args.get('order')
 
-#
-# @app.route('/report/drivers/?driver_id=SVF')
-# def index():
-#     return render_template('info_about_a_driver.html')
-#
-#
-# @app.route('/drivers/?order=desc')
-# def index():
-#     return render_template('order_desc.html')
+    if None is not driver_id:
+        return str(fine(driver_id))
+    elif None is not order:
+        if order == 'desc':
+            return render_template('drivers.html', title='List of drivers name and code',
+                                   diction=dict_all_info_reverse)
+    elif None is driver_id:
+        return render_template('drivers.html', title='List of drivers name and code',
+                               diction=dict_all_info)
+
+
+@app.route('/page/', methods=['GET', 'POST'])
+def page():
+    driver_id = request.args.get('driver_id')
+    if None is not driver_id:
+        return str(fine(driver_id))
 
 
 if __name__ == '__main__':
