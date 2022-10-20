afd4b747813c6cebb0b66a3e3fd8a2c0782ef103
@foxstudent102022 <vyacheslavseregin21@gmail.com>
2022-10-20 23:26:08 +0300

    add init

--- web_report.py
+++ web_report.py
@@ -14,7 +14,7 @@ def drivers_cod_name_dict(data):
     return parsed_lines
 
 
-def fine(k):
+def find(k):
     for dikts in dict_all_info:
         if dikts['driver_id'] == k:
             return dikts.get(k)
@@ -36,13 +36,13 @@ def drivers():
     driver_id = request.args.get('driver_id')
     order = request.args.get('order')
 
-    if None is not driver_id:
-        return str(fine(driver_id))
-    elif None is not order:
+    if driver_id is not None:
+        return str(find(driver_id))
+    elif order is not None:
         if order == 'desc':
             return render_template('drivers.html', title='List of drivers name and code',
-                                   diction=dict_all_info_reverse)
-    elif None is driver_id:
+                                   diction=sorted(dict_all_info, key=lambda d: d['driver_id'], reverse=True))
+    elif driver_id is None:
         return render_template('drivers.html', title='List of drivers name and code',
                                diction=dict_all_info)
 
@@ -51,7 +51,7 @@ def drivers():
 def page():
     driver_id = request.args.get('driver_id')
     if None is not driver_id:
-        return str(fine(driver_id))
+        return str(find(driver_id))
 
 
 if __name__ == '__main__':
