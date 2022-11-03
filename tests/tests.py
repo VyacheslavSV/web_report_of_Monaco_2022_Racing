32d1d599f8cc4e7a6e3d7e5cf0eba23b6e00497d
@foxstudent102022 <vyacheslavseregin21@gmail.com>
2022-11-03 12:40:13 +0200

    Вийшло, тести поки ні я поки не зрозумів як варити суп

---
+++ tests/tests.py
@@ -0,0 +1,17 @@
+import pytest
+
+from myproject import create_app
+
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Hello, World!'
+
+
+if __name__ == "__main__":
+    pytest.main()
