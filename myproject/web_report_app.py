0cb803e46b4ec7f5086c90a913d27446f30a1d47
@foxstudent102022 <vyacheslavseregin21@gmail.com>
2022-11-04 12:32:42 +0200

    1

--- myproject/web_report_app.py
+++ myproject/web_report_app.py
@@ -23,11 +23,12 @@ def report():
 def driver():
     order = request.args.get('order')
     files = request.args.get('files', FILES)
-    result = build_report(files, order)
     driver_id = request.args.get('driver_id')
-
+    result = build_report(files, order)
     if driver_id:
         driver_data = find_driver(result, driver_id)
         render_template('driver.html', driver_data=driver_data)
     return render_template('drivers.html', driver_data=result)
+
+
 
